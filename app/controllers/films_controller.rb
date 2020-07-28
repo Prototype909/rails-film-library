910ecf857aadc936fa71b54fb4a35284c56c9aea
Steve Anderson <prototype909@msn.com>
2020-07-27 18:55:40 -0600

    build out controllers

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,6 @@
 class FilmsController < ApplicationController
+    before_action :find_film, :redirect_if_not_user, only: [:show, :edit, :update, :destroy]
+    layout 'film'
 
     def index
     end
@@ -7,6 +9,14 @@ class FilmsController < ApplicationController
     end
 
     def create
+        @film = current_user.films.build(film_params)
+        if @film.valid?
+            @film.save
+            redirect_to film_path(@film)
+        else
+            @film = Film.find_by_id(params[:film_id]) if params[:film_id]
+            render :new
+        end
     end
 
     def edit
@@ -24,11 +34,16 @@ class FilmsController < ApplicationController
     private
 
     def film_params
-    end
-
-    def set_film
+        params.require(:film).permit(:title, :director, :year, :description)
     end
 
     def redirect_if_not_user
+        if @film.user != current_user
+            redirect_to user_path(current_user), alert: "You Do Not Have Permission To Edit This Film!"
+        end
+    end
+
+    def find_film
+        @film = Film.find(params[:id])
     end
 end
--- db/migrate/20200727212720_create_films.rb
+++ db/migrate/20200727212720_create_films.rb
@@ -3,6 +3,8 @@ class CreateFilms < ActiveRecord::Migration[6.0]
     create_table :films do |t|
       t.string :title
       t.string :director
+      t.integer :year
+      t.text :description
       
       t.references :user, null: false, foreign_key: true
       t.references :genre, null: false, foreign_key: true
