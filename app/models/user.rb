44c950d374e0f17019718351ff1cc6ea6979fd8a
Steve Anderson <prototype909@msn.com>
2020-07-28 16:15:03 -0600

    create helper add nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :films
-    has_many :received_comments, through: :films, source: comments
+    has_many :received_comments, through: :films, source: :comments
     has_many :comments
-    has_many :commented_films, through: comments, source: film
+    has_many :commented_films, through: :comments, source: :film
 
-    validates :username, email, presence: true
+    validates :username, :email, presence: true
     validates :email, uniqueness: true
     validates :password, confirmation: true
     validates :password_confirmation, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'welcome#home'
+
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/login' => 'session#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'session#destroy'
+
   
   resources :users, only: [:new, :create, :show]
   resources :comments
