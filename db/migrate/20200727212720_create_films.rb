ba09b9d9416ba46c9d2e97805ade9b20d58aaa90
Steve Anderson <prototype909@msn.com>
2020-07-27 18:03:22 -0600

    added migration data

--- db/migrate/20200727212720_create_films.rb
+++ db/migrate/20200727212720_create_films.rb
@@ -2,7 +2,11 @@ class CreateFilms < ActiveRecord::Migration[6.0]
   def change
     create_table :films do |t|
       t.string :title
+      t.string :director
+      
       t.references :user, null: false, foreign_key: true
+      t.references :genre, null: false, foreign_key: true
+      t.references :comment, null: false, foreign_key: true
 
       t.timestamps
     end
