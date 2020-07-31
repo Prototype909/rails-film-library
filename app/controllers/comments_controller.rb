6deee82d4934fc07798f43c8616390b6f189789e
Steve Anderson <prototype909@msn.com>
2020-07-30 20:04:15 -0600

    have comments show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,27 @@ class CommentsController < ApplicationController
     before_action :find_comment, :redirect_if_not_user, only: [:show, :edit, :update, :destroy]
 
     def index
-        @comments = Comment.org
-        if params[:film_id]
-           @comments = Film.find(params[:film_id]).comments
-        else
-          @comments = Comment.all
-        end
+        @user = User.find_by(id: params[:user_id])
+        if @user 
+            @comments = @user.comments 
+        else 
+            @comments = Comment.all
+        end 
     end
 
     def new
+        @comment = Comment.new(film_id: params[:film_id])
     end
 
     def create
+        @film = Film.find(params[:film_id])
+        @comment = @film.comments.create(comment_params)
+        @comment.user.id = current_user.id
+        if @comment.save
+            redirect_to film_path(@film)
+        else 
+            redirect_to film_path(film)
+        end
     end
 
     def show
@@ -21,12 +30,28 @@ class CommentsController < ApplicationController
     end
 
     def edit
+        @comment = Comment.find_by(id: params[:id])
     end
 
     def update
+        @comment = Comment.find_by(id: params[:id])
+        if @comment.user == current_user
+            @film.comment.update(comment_params)
+            redirect_to comment_path(@comment)
+        else 
+            render :edit 
+        end
+
     end
 
     def destroy
+        @comment = Comment.find_by(id: params[:id])
+
+        if @comment.user.id == current_user.id 
+            @comment.destroy 
+        else 
+            redirect_to films_path 
+        end
     end
 
 
@@ -34,6 +59,7 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
+        params.require(:comment).permit(:body, :film_id)
     end
 
     def find_comment
@@ -42,4 +68,3 @@ class CommentsController < ApplicationController
 end
 
 
-#nest comments under fims!!!
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :film
+    accepts_nested_attributes_for :user
 
-    validates :body, presense: true
+    # def user_attributes=(user_attributes)
+    #     self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+    # end
+
+    validates :body, presence: true
 end
